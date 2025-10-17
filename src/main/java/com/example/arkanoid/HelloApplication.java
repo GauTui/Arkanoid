305d5f37ae3cf0c1266adfff01e5a7ed40b7818f
copycat <tuanduc03102006@gmail.com>
2025-10-18 00:51:19 +0700

    oke

--- src/main/java/com/example/arkanoid/HelloApplication.java
+++ src/main/java/com/example/arkanoid/HelloApplication.java
@@ -4,10 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -15,9 +13,16 @@ import java.io.File;
 
 import java.io.IOException;
 
+
+/**
+ * cái này là để chạy start game nhé ae!!!!!
+ */
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        //tạo các nút chức năng trong phần start game
         Button hu1 = new Button("start game");
         hu1.setLayoutX(205);
         hu1.setLayoutY(330);
@@ -31,20 +36,36 @@ public class HelloApplication extends Application {
         hu3.setLayoutX(205);
         hu3.setLayoutY(460);
 
+        //tạo nút hướng dẫn người chơi
         Button tutorial = new Button("huong dan");
         tutorial.setLayoutX(560);
         tutorial.setLayoutY(560);
         tutorial.setPrefSize(60, 60);
 
-        File huhu = new File("\"C:\\Users\\Gold D Rophi\\Downloads\\huhu.png\"");
+
+        //tạo file để load ảnh nền
+        File huhu = new File("C:\\Users\\Gold D Rophi\\Downloads\\huhu.png"); //ảnh này là nền nhé, anh em chỉnh thành ảnh gì cũng được
+
+        //tạo ảnh nền cho phần start game
         Image anhnen = new Image(huhu.toURL().toString());
+        ImageView bgView = new ImageView (anhnen);
+        //set độ dài rộng của ảnh nền
+        bgView.setFitWidth(640);
+        bgView.setFitHeight(600);
+        //dòng này để chọn xem có bóp méo ảnh để lấy đúng tỷ lệ hay không!!
+        bgView.setPreserveRatio(false);
 
 
-        Pane root = new Pane();
-        root.getChildren().addAll(hu1, hu2, hu3, tutorial);
+        StackPane root = new StackPane();
+        Pane bt = new Pane();
+        //thêm các nút button làm con của bt
+        bt.getChildren().addAll(hu1, hu2, hu3, tutorial);
+        //thêm bt làm con của root để chỉ cần chạy root
+        root.getChildren().addAll(bgView, bt);
 
+        //load khung hình start game với từng tỷ lệ
         Scene scene = new Scene(root, 640, 640);
-
+        // tiêu đề của game
         stage.setTitle("Arkanoid Game - Team 6");
 
         stage.setScene(scene);
