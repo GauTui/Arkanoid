adf77f0ff873cbab44afe2145eafb1c319eca560
copycat <tuanduc03102006@gmail.com>
2025-10-18 23:36:28 +0700

    oke

--- src/main/java/com/example/arkanoid/Brick/Brick.java
+++ src/main/java/com/example/arkanoid/Brick/Brick.java
@@ -1,4 +1,25 @@
 package com.example.arkanoid.Brick;
 
 public class Brick {
+    protected int hitPoints;
+    protected boolean isDestroyed = false;
+
+    public static final int BRICK_WIDTH = 60;
+    public static final int BRICK_HEIGHT = 60;
+
+    public Brick(double x, double y, int width, int height, int hitPoints) {
+        //super(x, y, width, height);
+        this.hitPoints = hitPoints;
+        this.view = new Rectangle(x, y, width, height);
+    }
+    public void takeHit() {
+        hitPoints--;
+        if(hitPoints <= 0) {
+            isDestroyed = true;
+            this.view = null;
+        }
+    }
+    public boolean isDestroyed() {
+        return isDestroyed;
+    }
 }
--- src/main/java/com/example/arkanoid/Brick/StrongBrick.java
+++ src/main/java/com/example/arkanoid/Brick/StrongBrick.java
@@ -1,4 +1,18 @@
 package com.example.arkanoid.Brick;
 
-public class StrongBrick {
+public class StrongBrick extends Brick{
+    public StrongBrick(double x, double y, int width, int height, int hitPoints) {
+        super(x,y,width,height,hitPoints);
+    }
+
+    @Override
+    public void takeHit() {
+        hitPoints--;
+        if(hitPoints <=0 ) {
+            isDestroyed = true;
+            this.view = null;
+        }
+    }
+
+
 }
