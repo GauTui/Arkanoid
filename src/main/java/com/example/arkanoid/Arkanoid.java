4b8820311fc5faeb418cd4ce265870ea2dee89b3
copycat <tuanduc03102006@gmail.com>
2025-11-04 19:14:16 +0700

    anh em nao ranh thi sua lai address image ho nhe :))

--- src/main/java/com/example/arkanoid/Arkanoid.java
+++ src/main/java/com/example/arkanoid/Arkanoid.java
@@ -280,21 +280,22 @@ public class Arkanoid extends Application {
     public void openLevelSelect(Stage stage, Scene menuScene) throws IOException {
         Pane SelectLVButton = new Pane();
         StackPane SelectLV = new StackPane();
+
         Image image = new Image("file:src/main/resources/com/example/arkanoid/images/SelectLVImg.png");
         ImageView imageView = new ImageView(image);
         imageView.setViewport(new Rectangle2D(0, 0, 920, 863));
-        AnimationGame anim = new AnimationGame(imageView, 4, 920, 863, 600);
+        AnimationGame anim = new AnimationGame(imageView, 4, 1056, 992, 600);
         anim.play();
 
 
         File LoadImgLV1 = new File("src/main/resources/com/example/arkanoid/images/Level1Button.png"); // ở đây sẽ thêm địa chỉ của ảnh muốn render ra khi mà vẽ
         Image LV1Img = new Image(LoadImgLV1.toURL().toString());
         ImageView LV1ImgV = new ImageView(LV1Img);
-        LV1ImgV.setFitHeight(60); // set chieu cao
+        LV1ImgV.setFitHeight(200); // set chieu cao
         LV1ImgV.setFitWidth(310); // set chiều rong
         LV1ImgV.setPickOnBounds(false);
         LV1ImgV.setLayoutX(30); // tọa độ X của đầu nút
-        LV1ImgV.setLayoutY(180);
+        LV1ImgV.setLayoutY(100);
         LV1ImgV.setOnMouseEntered(e -> {
             ScaleTransition st = new ScaleTransition(Duration.millis(150), LV1ImgV);
             st.setToX(1.1);
@@ -319,11 +320,11 @@ public class Arkanoid extends Application {
         File LoadImgLV2 = new File("src/main/resources/com/example/arkanoid/images/Level2Button.png"); // ở đây sẽ thêm địa chỉ của ảnh muốn render ra khi mà vẽ
         Image LV2Img = new Image(LoadImgLV2.toURL().toString());
         ImageView LV2ImgV = new ImageView(LV2Img);
-        LV2ImgV.setFitHeight(60); // set chieu cao
+        LV2ImgV.setFitHeight(200); // set chieu cao
         LV2ImgV.setFitWidth(310); // set chiều rong
         LV2ImgV.setPickOnBounds(false);
         LV2ImgV.setLayoutX(30); // tọa độ X của đầu nút
-        LV2ImgV.setLayoutY(260);
+        LV2ImgV.setLayoutY(200);
 
         LV2ImgV.setOnMouseEntered(e -> {
             ScaleTransition st = new ScaleTransition(Duration.millis(150), LV2ImgV);
@@ -349,11 +350,11 @@ public class Arkanoid extends Application {
         File LoadImgLV3 = new File("src/main/resources/com/example/arkanoid/images/Level3Button.png"); // ở đây sẽ thêm địa chỉ của ảnh muốn render ra khi mà vẽ
         Image LV3Img = new Image(LoadImgLV3.toURL().toString());
         ImageView LV3ImgV = new ImageView(LV3Img);
-        LV3ImgV.setFitHeight(60); // set chieu cao
+        LV3ImgV.setFitHeight(200); // set chieu cao
         LV3ImgV.setFitWidth(310); // set chiều rong
         LV3ImgV.setPickOnBounds(false);
         LV3ImgV.setLayoutX(30); // tọa độ X của đầu nút
-        LV3ImgV.setLayoutY(340);
+        LV3ImgV.setLayoutY(300);
         LV3ImgV.setOnMouseEntered(e -> {
             ScaleTransition st = new ScaleTransition(Duration.millis(150), LV3ImgV);
             st.setToX(1.1);
@@ -378,11 +379,11 @@ public class Arkanoid extends Application {
         File LoadImgLV4 = new File("src/main/resources/com/example/arkanoid/images/Level4Button.png"); // ở đây sẽ thêm địa chỉ của ảnh muốn render ra khi mà vẽ
         Image LV4Img = new Image(LoadImgLV4.toURL().toString());
         ImageView LV4ImgV = new ImageView(LV4Img);
-        LV4ImgV.setFitHeight(60); // set chieu cao
+        LV4ImgV.setFitHeight(200); // set chieu cao
         LV4ImgV.setFitWidth(310); // set chiều rong
         LV4ImgV.setPickOnBounds(false);
         LV4ImgV.setLayoutX(30); // tọa độ X của đầu nút
-        LV4ImgV.setLayoutY(420);
+        LV4ImgV.setLayoutY(400);
         LV4ImgV.setOnMouseEntered(e -> {
             ScaleTransition st = new ScaleTransition(Duration.millis(150), LV4ImgV);
             st.setToX(1.1);
@@ -407,7 +408,7 @@ public class Arkanoid extends Application {
         File LoadImgLV5 = new File("src/main/resources/com/example/arkanoid/images/Level5Button.png"); // ở đây sẽ thêm địa chỉ của ảnh muốn render ra khi mà vẽ
         Image LV5Img = new Image(LoadImgLV5.toURL().toString());
         ImageView LV5ImgV = new ImageView(LV5Img);
-        LV5ImgV.setFitHeight(60); // set chieu cao
+        LV5ImgV.setFitHeight(200); // set chieu cao
         LV5ImgV.setFitWidth(310); // set chiều rong
         LV5ImgV.setPickOnBounds(false);
         LV5ImgV.setLayoutX(30); // tọa độ X của đầu nút
@@ -433,34 +434,6 @@ public class Arkanoid extends Application {
             startLevel(stage, 5);
         });
 
-        File LoadImgLV6 = new File("src/main/resources/com/example/arkanoid/images/Level6Button.png"); // ở đây sẽ thêm địa chỉ của ảnh muốn render ra khi mà vẽ
-        Image LV6Img = new Image(LoadImgLV6.toURL().toString());
-        ImageView LV6ImgV = new ImageView(LV6Img);
-        LV6ImgV.setFitHeight(60); // set chieu cao
-        LV6ImgV.setFitWidth(310); // set chiều rong
-        LV6ImgV.setPickOnBounds(false);
-        LV6ImgV.setLayoutX(30); // tọa độ X của đầu nút
-        LV6ImgV.setLayoutY(580);
-        LV6ImgV.setOnMouseEntered(e -> {
-            ScaleTransition st = new ScaleTransition(Duration.millis(150), LV6ImgV);
-            st.setToX(1.1);
-            st.setToY(1.1);
-            st.play();
-        });
-
-        LV6ImgV.setOnMouseExited(e -> {
-            ScaleTransition st = new ScaleTransition(Duration.millis(150), LV6ImgV);
-            st.setToX(1.0);
-            st.setToY(1.0);
-            st.play();
-        });
-
-        LV6ImgV.setOnMouseClicked(e -> {
-            Stage stage6 = (Stage) ((Node)e.getSource()).getScene().getWindow();
-            SoundManager lv6sound = new SoundManager();
-            SoundManager.playBackgroundMusic("src/main/resources/com/example/arkanoid/sounds/gameMusic1.wav");
-            startLevel(stage, 6);
-        });
 
         File LoadImg4 = new File("src/main/resources/com/example/arkanoid/images/BackButton.png"); // ở đây sẽ thêm địa chỉ của ảnh muốn render ra khi mà vẽ
         Image MainMenuImg = new Image(LoadImg4.toURL().toString());
@@ -491,7 +464,7 @@ public class Arkanoid extends Application {
                 ex.printStackTrace();
             }
         });
-        SelectLVButton.getChildren().addAll(LV1ImgV, LV2ImgV, LV3ImgV, LV4ImgV, LV5ImgV,LV6ImgV, MainMenuImgV);
+        SelectLVButton.getChildren().addAll(LV1ImgV, LV2ImgV, LV3ImgV, LV4ImgV, LV5ImgV, MainMenuImgV);
         SelectLV.getChildren().addAll(imageView, SelectLVButton);
         Scene lvScene = new Scene(SelectLV, 920, 720);
         stage.setScene(lvScene);
